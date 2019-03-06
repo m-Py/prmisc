105f633bafce5276056ed15b3dee35be0d1bd40d
einGlasRotwein <juliane.tkotz@gmail.com>
2019-03-06 13:33:16 +0100

    export needed for usage

--- R/print_wilcoxon_rs.R
+++ R/print_wilcoxon_rs.R
@@ -78,6 +78,7 @@
 #'                   groupvar = dat$Species)
 #'
 #' @author Juliane Tkotz \email{juliane.tkotz@@hhu.de}
+#' @export
 #'
 print_wilcoxon_rs <- function(wc_object, decimals_p = 3, consistent = NULL,
                               group1 = NULL, group2 = NULL, groupvar = NULL) {
