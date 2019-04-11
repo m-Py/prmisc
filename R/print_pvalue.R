c61f29612e8c8808f0687bbcf0a6b3fa29d203df
Martin Papenberg <martin.papenberg@hhu.de>
2019-04-11 12:47:19 +0200

    implemented numbers_only argument proposed by @einGlasRotwein

--- R/print_pvalue.R
+++ R/print_pvalue.R
@@ -4,6 +4,10 @@
 #' 
 #' @param pvalues The p-values
 #' @param decimals The number of decimals to be printed
+#' @param numbers_only An integer indicating whether the p-values
+#'                     should be printed whithout the accompanying p.
+#'                     Defaults to \code{FALSE}. 
+#'
 #' @return A string representation of the p value to be used in Rmarkdown
 #'   documents.
 #' 
@@ -19,13 +23,14 @@
 #' # Format several p-values with one function call
 #' format_p(c(0.3123, 0.001, 0.00001, 0.19))
 #' format_p(c(.999, .9999, 1))
+#' format_p(c(0.3123, 0.001, 0.00001, 0.19, .99999), numbers_only = TRUE)
 #'
 
-format_p <- function(pvalues, decimals = 3) {
-  return(vectorize_print(pvalues, decimals, format_p_))
+format_p <- function(pvalues, decimals = 3, numbers_only = FALSE) {
+  return(vectorize_print(pvalues, decimals, format_p_, numbers_only))
 }
 
-format_p_ <- function(pvalue, decimals) {
+format_p_ <- function(pvalue, decimals, numbers_only) {
   if (pvalue < 0 | pvalue > 1)
     stop("p value is smaller than 0 or larger than 1")
   if (pvalue >= 0.001) {
@@ -37,5 +42,10 @@ format_p_ <- function(pvalue, decimals) {
   }
   if (pvalue < 0.01 & decimals <= 2) p <- "$p < .01$"
   if (pvalue < 0.001) p <- "$p < .001$"
+  if (numbers_only) {
+    p <- gsub("p = ", "", p)
+    p <- gsub("p ", "", p)
+    return(p)
+  }
   return(p)
 }
